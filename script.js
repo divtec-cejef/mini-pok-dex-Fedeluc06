6944fee712fa2f0e7173c2a92ac3b5475e26b3dd
Fedeluc06 <luca.fedele@divtec.ch>
2024-09-16 22:47:59 +0200

    Étape 4 terminée

--- script.js
+++ script.js
@@ -7,9 +7,7 @@
 'use strict';
 
 // Couleur par défaut pour les types de Pokémon non définis
-const DEFAULT_COLOR = '#ccc';
-
-const pokemonContainer = document.querySelector(".pokemon-container");
+const DEFAULT_COLOR = 'red';
 
 // Couleurs pour chaque type de Pokémon
 const typeColors = {
@@ -30,79 +28,72 @@ const typeColors = {
 };
 
 // Tableau d'objets représentant les Pokémon
-
 const pokemonsTab = [
-    {name: 'Bulbizarre', type: 'Plante,Poison', level: 15, img: 'bulbizarre.png'},
-    {name: 'Salamèche', type: 'Feu', level: 20, img: 'salameche.png'},
-    {name: 'Carapuce', type: 'Eau', level: 10, img: 'carapuce.png'},
-    {name: 'Rondoudou', type: 'Normal,Fée', level: 25, img: 'rondoudou.png'},
-    {name: 'Ectoplasma', type: 'Spectre,Poison', level: 45, img: 'ectoplasma.png'},
-    {name: 'Évoli', type: 'Normal,Combat', level: 22, img: 'evoli.png'},
-    {name: 'Dracaufeu', type: 'Feu,Vol', level: 50, img: 'dracaufeu.png'},
-    {name: 'Florizarre', type: 'Plante,Poison', level: 55, img: 'florizarre.png'},
-    {name: 'Tortank', type: 'Eau', level: 52, img: 'tortank.png'},
-    {name: 'Mélofée', type: 'Fée', level: 18, img: 'melofee.png'},
-    {name: 'Raichu', type: 'Électrique', level: 40, img: 'raichu.png'},
-    {name: 'Magicarpe', type: 'Eau', level: 5, img: 'magicarpe.png'},
-    {name: 'Lokhlass', type: 'Eau,Glace', level: 35, img: 'lokhlass.png'},
-    {name: 'Onix', type: 'Roche,Sol', level: 30, img: 'onix.png'},
-    {name: 'Ronflex', type: 'Normal', level: 45, img: 'ronflex.png'},
-    {name: 'Mewtwo', type: 'Psy', level: 70, img: 'mewtwo.png'},
-    {name: 'Pikachu', type: 'Électrique', level: 35, img: 'pikachu.png'}
+    { name: 'Pikachu', type: 'Électrique', level: 35, img: 'pikachu.png' },
+    { name: 'Bulbizarre', type: 'Plante,Poison', level: 15, img: 'bulbizarre.png' },
+    { name: 'Salamèche', type: 'Feu', level: 20, img: 'salameche.png' },
+    { name: 'Carapuce', type: 'Eau', level: 10, img: 'carapuce.png' },
+    { name: 'Rondoudou', type: 'Normal,Fée', level: 25, img: 'rondoudou.png' },
+    { name: 'Ectoplasma', type: 'Spectre,Poison', level: 45, img: 'ectoplasma.png' },
+    { name: 'Évoli', type: 'Normal,Combat', level: 22, img: 'evoli.png' },
+    { name: 'Dracaufeu', type: 'Feu,Vol', level: 50, img: 'dracaufeu.png' },
+    { name: 'Florizarre', type: 'Plante,Poison', level: 55, img: 'florizarre.png' },
+    { name: 'Tortank', type: 'Eau,toto', level: 52, img: 'tortank.png' },
+    { name: 'Mélofée', type: 'Fée', level: 18, img: 'melofee.png' },
+    { name: 'Raichu', type: 'Électrique', level: 40, img: 'raichu.png' },
+    { name: 'Magicarpe', type: 'Eau', level: 5, img: 'magicarpe.png' },
+    { name: 'Lokhlass', type: 'Eau,Glace', level: 35, img: 'lokhlass.png' },
+    { name: 'Onix', type: 'Roche,Sol', level: 30, img: 'onix.png' },
+    { name: 'Ronflex', type: 'Normal', level: 45, img: 'ronflex.png' },
+    { name: 'Mewtwo', type: 'Psy', level: 70, img: 'mewtwo.png' }
 ];
 
 /**
- * Genere le code HTML pour les différentes cartes pokemons
- * @param pokemon à afficher
- * @returns {string} le code HTML permettant d'afficher le pokemon.
+ * Fonction qui retourne le code HTML de la carte du pokémon passé en paramètre
+ * @param pokemon { name: 'Mewtwo', type: 'Psy', level: 70, img: 'mewtwo.png' } un objet Pokémon
  */
-function generatePokemonCardHTML(pokemon) {
-    let cardHTML = "<div class='pokemon-card' style='background: "
-    let listeTypes = pokemon['type'].split(',');
+function generatePokemonCardHTML(pokemon){
+    let tabType = pokemon.type.split(',');
+    let type1 = tabType[0];
+    let type2 = tabType[1];
+    let bgColor = typeColors[type1];
 
-    // Ajout couleur du Background
-    if (listeTypes.length > 1) {
-        cardHTML += "linear-gradient(to right, ";
-        for (let index = 0; index < listeTypes.length; index++) {
-            if (index !== 0)
-                cardHTML += ",";
-            cardHTML += typeColors[listeTypes[index]] + " 50%";
-        }
-        cardHTML += ")"
-    } else {
-        cardHTML += typeColors[pokemon['type']];
+    // Si un deuxième type existe
+    if(type2) {
+        let t2 = typeColors[type2] || DEFAULT_COLOR;
+        bgColor = `linear-gradient(to right, ${typeColors[type1]} 50%, ${t2} 50%);`;
     }
-
-    cardHTML += ";'> <img src='images/" + pokemon['img'] +
-        "' alt='" + pokemon['name'] + "'> <h2>" + pokemon['name'] + "</h2> <div>Type : ";
-
-    // écriture des types de pokemons
-    for (let index = 0; index < listeTypes.length; index++) {
-        if (index !== 0) {
-            cardHTML += " / ";
-        }
-        cardHTML += listeTypes[index];
-    }
-
-    return cardHTML + "</div> <div>Niveau: " + pokemon['level'] + "</div> </div>";
+    return `<div class="pokemon-card"
+                 style="background: ${bgColor};"
+            >
+                <img src="images/${pokemon.img}" alt="${pokemon.name}">
+                <h2>${pokemon.name}</h2>
+                <div>Type: ${pokemon.type.replace(',', ' / ')}</div>
+                <div>Niveau: ${pokemon.level}</div>
+            </div>`;
 }
 
 /**
- * affiche les pokemons
+ * Affiche le nom des pokémons dans la <div class="pokemon-container">
  */
-function displayPokemons() {
-    pokemonContainer.textContent = "";
-
-    if (!pokemonsTab.length) {
-        pokemonContainer.innerHTML = '<p>Dracaufeu a tout brûlé, aucun Pokémon ne correspond à ta recherche !</p>'
-        return;
+function displayPokemons () {
+    // Récupération de l'élément HTML <div class="pokemon-container">
+    const divContainer = document.querySelector('.pokemon-container');
+    // Vide le contenu du container
+    divContainer.innerHTML = '';
+    // Si tableau est vide, si la taille est de 0
+    if(!pokemonsTab.length) {
+        divContainer.innerHTML = "<p>Dracaufeu a tout brûlé, aucun Pokémon ne correspond à ta recherche !</p>";
+        return; // Sort de la fonction
     }
-
     let resHTML = '';
+    // Pour chaque pokémon du tableau pokemons
     for (let pokemon of pokemonsTab) {
         resHTML += generatePokemonCardHTML(pokemon);
     }
-    pokemonContainer.innerHTML = resHTML;
+    // Ajoute les cartes au container
+    divContainer.innerHTML = resHTML;
 }
 
+// Appelle la fonction displayPokemons()
 displayPokemons();
